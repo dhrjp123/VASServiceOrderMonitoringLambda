70449b07e92332c2f2e8868243ccb5ff53aa5d27
Aayushman Mishra <45511987+mishraaayushman3@users.noreply.github.com>
2020-06-13 13:55:39 +0530

    worked on wraping code within 120 chars per line

--- VASServiceOrderMonitoringLambda/ServiceMonitoringFunction/src/com.amazon.vas.serviceordermonitoringlambda/activity/GetJobMetricsActivity.java
+++ VASServiceOrderMonitoringLambda/ServiceMonitoringFunction/src/com.amazon.vas.serviceordermonitoringlambda/activity/GetJobMetricsActivity.java
@@ -9,23 +9,28 @@ import com.google.inject.Inject;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor(onConstructor = @__({ @Inject}))
+@RequiredArgsConstructor(onConstructor = @__({@Inject}))
 public class GetJobMetricsActivity {
 
     @NonNull
     private final GetJobMetricsComponent getJobMetricsComponent;
 
-    public GetJobMetricsOutput enact(@NonNull final GetJobMetricsInput getJobMetricsInput){
+    public GetJobMetricsOutput enact(@NonNull final GetJobMetricsInput getJobMetricsInput) {
 
         final GetJobMetricsInputBO getJobMetricsInputBO = buildGetJobMetricsInputBO(getJobMetricsInput);
         final GetJobMetricsOutputBO getJobMetricsOutputBO = getJobMetricsComponent.getJobMetrics(getJobMetricsInputBO);
         final GetJobMetricsOutput getJobMetricsOutput = buildGetJobMetricsOutput(getJobMetricsOutputBO);
         return getJobMetricsOutput;
     }
-    private GetJobMetricsInputBO buildGetJobMetricsInputBO(GetJobMetricsInput getJobMetricsInput){
-        return GetJobMetricsInputBO.builder().city(getJobMetricsInput.getCity()).merchantId(getJobMetricsInput.getMerchantId()).groupingCriteria(getJobMetricsInput.getGroupingCriteria()).build();
+
+    private GetJobMetricsInputBO buildGetJobMetricsInputBO(GetJobMetricsInput getJobMetricsInput) {
+        return GetJobMetricsInputBO.builder().city(getJobMetricsInput.getCity())
+                .merchantId(getJobMetricsInput.getMerchantId())
+                .groupingCriteria(getJobMetricsInput.getGroupingCriteria()).build();
     }
-    private GetJobMetricsOutput buildGetJobMetricsOutput(GetJobMetricsOutputBO getJobMetricsOutputBO){
-        return GetJobMetricsOutput.builder().metaData(getJobMetricsOutputBO.getMetaData()).jobMetricsMap(getJobMetricsOutputBO.getJobMetricsMap()).build();
+
+    private GetJobMetricsOutput buildGetJobMetricsOutput(GetJobMetricsOutputBO getJobMetricsOutputBO) {
+        return GetJobMetricsOutput.builder().metaData(getJobMetricsOutputBO.getMetaData())
+                .jobMetricsMap(getJobMetricsOutputBO.getJobMetricsMap()).build();
     }
 }
