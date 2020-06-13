b093d7495a14a1c47b1fafcd003b0f97e9ee8d94
Aayushman Mishra <45511987+mishraaayushman3@users.noreply.github.com>
2020-06-13 13:54:35 +0530

    Updated accessor

--- VASServiceOrderMonitoringLambda/ServiceMonitoringFunction/src/com.amazon.vas.serviceordermonitoringlambda/accessor/ElasticSearchAccessor.java
+++ VASServiceOrderMonitoringLambda/ServiceMonitoringFunction/src/com.amazon.vas.serviceordermonitoringlambda/accessor/ElasticSearchAccessor.java
@@ -17,53 +17,57 @@ import org.elasticsearch.search.SearchHits;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.amazon.vas.serviceordermonitoringlambda.util.ApplicationConstants.searchScrollTimeout;
 
 @Log4j2
-@RequiredArgsConstructor(onConstructor = @__({ @Inject}))
+@RequiredArgsConstructor(onConstructor = @__({@Inject}))
 public class ElasticSearchAccessor {
-        private final RestHighLevelClient elasticSearchClient;
-        public List<String> getRecords(@NonNull SearchRequest searchRequest){
-                SearchResponse searchResponse = null;
-                try {
-                        searchResponse = elasticSearchClient.search(searchRequest, RequestOptions.DEFAULT);
-                } catch(final Exception e){
-                        throw new DependencyFailureException(e);
-                }
-                String scrollId = searchResponse.getScrollId();
-                SearchHits hits = searchResponse.getHits();
 
-                int docsCount = 0;
-                final List<String> queryVals = new ArrayList<>();
-                while(hits.getHits() != null && hits.getHits().length > 0){
-                        docsCount = hits.getHits().length;
-                        for(int i = 0; i < docsCount; i++) {
-                                queryVals.add(hits.getHits()[i].getSourceAsString());
-                        }
+    @NonNull
+    private final RestHighLevelClient elasticSearchClient;
+    private static final long SEARCH_SCROLL_TIMEOUT = 1L;
 
-                        SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollId);
-                        scrollRequest.scroll(TimeValue.timeValueMinutes(searchScrollTimeout));
-                        SearchResponse searchScrollResponse = null;
-                        try {
-                                searchScrollResponse = elasticSearchClient.scroll(scrollRequest, RequestOptions.DEFAULT);
-                        } catch(final Exception e){
-                                throw new DependencyFailureException(e);
-                        }
-                        scrollId = searchScrollResponse.getScrollId();
-                        hits = searchScrollResponse.getHits();
-                }
+    public List<String> getRecords(@NonNull SearchRequest searchRequest) {
 
-                clearScroll(scrollId);
-                return queryVals;
+        SearchResponse searchResponse = null;
+        try {
+            searchResponse = elasticSearchClient.search(searchRequest, RequestOptions.DEFAULT);
+        } catch (final Exception e) {
+            throw new DependencyFailureException(e);
+        }
+        String scrollId = searchResponse.getScrollId();
+        SearchHits hits = searchResponse.getHits();
+
+        int docsCount = 0;
+        final List<String> queryVals = new ArrayList<>();
+        while (hits.getHits() != null && hits.getHits().length > 0) {
+            docsCount = hits.getHits().length;
+            for (int i = 0; i < docsCount; i++) {
+                queryVals.add(hits.getHits()[i].getSourceAsString());
+            }
+
+            SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollId);
+            scrollRequest.scroll(TimeValue.timeValueMinutes(SEARCH_SCROLL_TIMEOUT));
+            SearchResponse searchScrollResponse = null;
+            try {
+                searchScrollResponse = elasticSearchClient.scroll(scrollRequest, RequestOptions.DEFAULT);
+            } catch (final Exception e) {
+                throw new DependencyFailureException(e);
+            }
+            scrollId = searchScrollResponse.getScrollId();
+            hits = searchScrollResponse.getHits();
         }
 
-        private void clearScroll(String scrollId){
-                final ClearScrollRequest clearScrollRequest = new ClearScrollRequest();
-                clearScrollRequest.addScrollId(scrollId);
-                try {
-                        elasticSearchClient.clearScroll(clearScrollRequest, RequestOptions.DEFAULT);
-                } catch(final Exception e){
-                        log.error(e.getMessage());
-                }
+        clearScroll(scrollId);
+        return queryVals;
+    }
+
+    private void clearScroll(String scrollId) {
+        final ClearScrollRequest clearScrollRequest = new ClearScrollRequest();
+        clearScrollRequest.addScrollId(scrollId);
+        try {
+            elasticSearchClient.clearScroll(clearScrollRequest, RequestOptions.DEFAULT);
+        } catch (final Exception e) {
+            log.error(e.getMessage());
         }
+    }
 }
