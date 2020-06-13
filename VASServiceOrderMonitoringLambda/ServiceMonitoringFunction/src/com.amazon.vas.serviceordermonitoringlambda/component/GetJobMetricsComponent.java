c5c923a82ef4505c22d12582ae7c1410a6246c09
Aayushman Mishra <45511987+mishraaayushman3@users.noreply.github.com>
2020-06-13 13:58:54 +0530

    capitalized enum values and created lambda function to find grouping criteria values

--- VASServiceOrderMonitoringLambda/ServiceMonitoringFunction/src/com.amazon.vas.serviceordermonitoringlambda/component/GetJobMetricsComponent.java
+++ VASServiceOrderMonitoringLambda/ServiceMonitoringFunction/src/com.amazon.vas.serviceordermonitoringlambda/component/GetJobMetricsComponent.java
@@ -9,20 +9,21 @@ import com.google.inject.Inject;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-@RequiredArgsConstructor(onConstructor = @__({ @Inject}))
+@RequiredArgsConstructor(onConstructor = @__({@Inject}))
 public class GetJobMetricsComponent {
 
     @NonNull
     private final JobDetailsBuilder jobDetailsbuilder;
 
-    public GetJobMetricsOutputBO getJobMetrics(@NonNull final GetJobMetricsInputBO getJobMetricsInputBO){
+    private static final String INVALID_GROUPING_CRITERIA = "Invalid Grouping Criteria";
+
+    public GetJobMetricsOutputBO getJobMetrics(@NonNull final GetJobMetricsInputBO getJobMetricsInputBO) {
         final List<JobDetailsBO> jobDetailsBOList = jobDetailsbuilder.getJobDetailsBuilder(getJobMetricsInputBO);
 
         final Function<JobDetailsBO, List<String>> CompositeKey = jobDetailsBO -> {
@@ -34,57 +35,68 @@ public class GetJobMetricsComponent {
 
         final Map<List<String>, JobAggregatedMetricsBO> jobMetricsMap = groupingFunctionJobDetailsMap
                 .entrySet().stream()
-                .collect(Collectors.toMap(e -> e.getKey() , e -> calculateJobAggregatedMetrics(e.getValue())));
+                .collect(Collectors.toMap(e -> e.getKey(), e -> calculateJobAggregatedMetrics(e.getValue())));
 
-        final Map<String,Map<String,String>> metaData = jobMetricsMap.entrySet().stream()
-                .collect(Collectors.toMap(e-> e.getKey().get(0),e -> getMetaData(e.getKey().get(0)),(value1,value2)->value1));
+        final Map<String, Map<String, String>> metaData = jobMetricsMap.entrySet().stream()
+                .collect(Collectors.toMap(e -> e.getKey().get(0), e -> getMetaData(e.getKey().get(0)),
+                        (value1, value2) -> value1));
 
         return GetJobMetricsOutputBO.builder().metaData(metaData).jobMetricsMap(jobMetricsMap).build();
     }
 
-    private Map<String,String> getMetaData(final String groupingEntity){
-        final Map<String,String> groupingEntityMetaData = new HashMap<String, String>(){{
-            put("name",groupingEntity);
-            put("ID",groupingEntity);
+    private Map<String, String> getMetaData(final String groupingEntity) {
+        final Map<String, String> groupingEntityMetaData = new HashMap<String, String>() {{
+            put("name", groupingEntity);
+            put("ID", groupingEntity);
         }};
         return groupingEntityMetaData;
     }
-    private JobAggregatedMetricsBO calculateJobAggregatedMetrics(final List<JobDetailsBO> jobDetailsBOList){
+
+    private JobAggregatedMetricsBO calculateJobAggregatedMetrics(final List<JobDetailsBO> jobDetailsBOList) {
         int etaDelayCount = 0;
         int otaFailureCount = 0;
         int statusNotUpdatedCount = 0;
-        final int totalJobsCount =  jobDetailsBOList.size();
+        final int totalJobsCount = jobDetailsBOList.size();
 
-        for(JobDetailsBO jobDetails : jobDetailsBOList){
-            etaDelayCount += (jobDetails.isEtaDelay()?1:0);
-            otaFailureCount += (jobDetails.isOtaFailure()?1:0);
-            statusNotUpdatedCount += ((jobDetails.getStatusNotUpdated().equals("NotServiced"))?1:0);
+        for (JobDetailsBO jobDetails : jobDetailsBOList) {
+            etaDelayCount += (jobDetails.isEtaDelay() ? 1 : 0);
+            otaFailureCount += (jobDetails.isOtaFailure() ? 1 : 0);
+            statusNotUpdatedCount += ((jobDetails.getStatusNotUpdated().equals("NotServiced")) ? 1 : 0);
         }
-        return JobAggregatedMetricsBO.builder().totalJobsCount(totalJobsCount).etaDelayCount(etaDelayCount).otaFailureCount(otaFailureCount).statusNotUpdatedCount(statusNotUpdatedCount).build();
-    };
-
-    private List<String> getGroupingCriteriaValues(JobDetailsBO jobDetailsBO, List<String> groupingCriteria){
-        final List<String> groupingCriteriaValues = new ArrayList<>();
-        for(String criterion : groupingCriteria){
-            groupingCriteriaValues.add(getGroupingCriteriaValue(criterion,jobDetailsBO));
-        }
-        return groupingCriteriaValues;
+        return JobAggregatedMetricsBO.builder().totalJobsCount(totalJobsCount).etaDelayCount(etaDelayCount)
+                .otaFailureCount(otaFailureCount).statusNotUpdatedCount(statusNotUpdatedCount).build();
     }
-    private String getGroupingCriteriaValue(String criterion, JobDetailsBO jobDetailsBO) throws RuntimeException{
+
+    private List<String> getGroupingCriteriaValues(JobDetailsBO jobDetailsBO, List<String> groupingCriteria) {
+        return groupingCriteria.stream().map(criterion -> getGroupingCriteriaValue(criterion, jobDetailsBO))
+                .collect(Collectors.toList());
+    }
+
+    private String getGroupingCriteriaValue(String criterion, JobDetailsBO jobDetailsBO) throws RuntimeException {
         String groupingCriteriaValue;
         GroupingCriteria CRITERION = GroupingCriteria.valueOf(criterion);
 
-        switch (CRITERION){
-            case city: groupingCriteriaValue = jobDetailsBO.getCity();break;
-            case merchantId: groupingCriteriaValue = jobDetailsBO.getMerchantId();break;
-            case technicianId: groupingCriteriaValue = jobDetailsBO.getTechnicianID();break;
-            case slotStartTime: groupingCriteriaValue = jobDetailsBO.getSlotStartTime();break;
-            default :throw new RuntimeException();
+        switch (CRITERION) {
+            case CITY:
+                groupingCriteriaValue = jobDetailsBO.getCity();
+                break;
+            case MERCHANT_ID:
+                groupingCriteriaValue = jobDetailsBO.getMerchantId();
+                break;
+            case TECHNICIAN_ID:
+                groupingCriteriaValue = jobDetailsBO.getTechnicianId();
+                break;
+            case SLOT_START_TIME:
+                groupingCriteriaValue = jobDetailsBO.getSlotStartTime();
+                break;
+            default:
+                throw new RuntimeException(INVALID_GROUPING_CRITERIA);
         }
         return groupingCriteriaValue;
     }
-    private enum GroupingCriteria{
-        city,merchantId,technicianId, slotStartTime;
+
+    private enum GroupingCriteria {
+        CITY, MERCHANT_ID, TECHNICIAN_ID, SLOT_START_TIME;
     }
 
 }
