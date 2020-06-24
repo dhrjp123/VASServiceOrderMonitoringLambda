63eeb1c83c081c710c4626a799b810305e1c204e
Aayushman Mishra <45511987+mishraaayushman3@users.noreply.github.com>
2020-06-24 19:35:12 +0530

    Add files via upload

---
+++ ServiceMonitoringFunction/tst/com.amazon.vas.serviceordermonitoringlambda/TestConstants.java
@@ -0,0 +1,9 @@
+package util;
+
+public class TestConstants {
+    public static String NOT_SERVICED = "NotServiced";
+    public static String CITY = "CITY";
+    public static String MERCHANT_ID = "MERCHANT_ID";
+    public static String SLOT_START_TIME = "SLOT_START_TIME";
+    public static String TEST_CITY = "City-5";
+}
